d4d57876abb298ee9317c73e08931db453578518
daeun.lee <nextree.59@partner.samsung.com>
2022-12-02 18:08:44 +0900

    fix reviews

--- src/main/java/com/example/todoexample/controller/TodoController.java
+++ src/main/java/com/example/todoexample/controller/TodoController.java
@@ -5,44 +5,41 @@ import com.example.todoexample.store.TodoDoc;
 import com.example.todoexample.store.domain.Todo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-// FIXME: Spring DI의 대표적인 두가지 방식 관점에서 @RequiredArgsConstructor의 의미와 @Autowired의 의미를 잘 파악해보세요.
 @RequiredArgsConstructor
-// FIXME: controller 가 여러 개로 늘어난다면...?
-@RequestMapping("/api")
-// FIXME: controller 가 여러 개로 늘어난다면...?
-@CrossOrigin(origins="*")
+@RequestMapping("/api/todo")
 public class TodoController {
 
-    @Autowired
     private final TodoService todoService;
 
-    // FIXME : Todo와 TodoDoC 각각의 의미를 다시 한번 생각해보세요.
-
     @PostMapping("/item")
-    public void createItem (@RequestBody TodoDoc todoDoc) {
-        todoService.createItem(todoDoc);
+    public void createItem (@RequestBody Todo todo) {
+        todoService.createItem(todo);
     }
 
-    // FIXME : Infinite Scroll을 적용해볼 수 있도록 API 를 재설계 해보세요.
     @GetMapping("/list")
-    public List<TodoDoc> getList () {
-        return todoService.getList();
+    public List<Todo> getList (@RequestParam int page, int limit) {
+        if(limit != 0) {
+            return todoService.getList(page, limit);
+        }else{
+            return todoService.getList();
+        }
     }
 
     @PutMapping("/item")
-    public void updateItem (@RequestBody TodoDoc todoDoc) {
-        todoService.updateItem(todoDoc);
+    public void updateItem (@RequestBody Todo todo) {
+        todoService.updateItem(todo);
     }
 
     @DeleteMapping("/item")
-    public void deleteItem (@RequestBody String id) {
-        todoService.deleteItem(id);
+    public void deleteItem (@RequestBody Todo todo) {
+        todoService.deleteItem(todo);
     }
 
 }
--- src/main/java/com/example/todoexample/service/TodoService.java
+++ src/main/java/com/example/todoexample/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.example.todoexample.service;
 
 import com.example.todoexample.store.TodoDoc;
+import com.example.todoexample.store.domain.Todo;
 import com.example.todoexample.store.repository.TodoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
@@ -13,22 +18,31 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
-    // FIXME : Todo와 TodoDoC 각각의 의미를 다시 한번 생각해보세요. (layered architecture)
+    private final ModelMapper modelMapper = new ModelMapper();
 
-    public void createItem(TodoDoc todoDoc) {
+    public void createItem(Todo todo) {
+        TodoDoc todoDoc = modelMapper.map(todo, TodoDoc.class);
         todoRepository.save(todoDoc);
     }
 
-    public List<TodoDoc> getList () {
-        return todoRepository.findAll();
+    public List<Todo> getList () {
+        List<TodoDoc> todoList = todoRepository.findAll();
+        return todoList.stream().map(item -> modelMapper.map(item, Todo.class)).collect(Collectors.toList());
     }
 
-    public void updateItem(TodoDoc todoDoc) {
+    public List<Todo> getList(int page, int limit) {
+        Page<TodoDoc> todoPage = todoRepository.findAll(PageRequest.of(page, limit));
+        return todoPage.stream().map(item -> modelMapper.map(item, Todo.class)).collect(Collectors.toList());
+    }
+
+    public void updateItem(Todo todo) {
+        TodoDoc todoDoc = modelMapper.map(todo, TodoDoc.class);
         todoRepository.save(todoDoc);
     }
 
-    public void deleteItem(String id) {
-        todoRepository.deleteById(id);
+    public void deleteItem(Todo todo) {
+        TodoDoc todoDoc = modelMapper.map(todo, TodoDoc.class);
+        todoRepository.delete(todoDoc);
     }
 
 }
